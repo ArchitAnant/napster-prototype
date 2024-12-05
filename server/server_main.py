cdb49cfacf8f1a1e4b0c59a4f0871e9318dccc0c
ArchitAnant <architanant5@gmail.com>
2024-12-05 10:47:10 +0530

    Testing server: One client

--- server/server_main.py
+++ server/server_main.py
@@ -2,8 +2,9 @@
 Server Main - Entry point for managing client registrations and peer discovery.
 """
 
-from server.server_registry import register_client, get_peer_list
+from server_registry import register_client, get_peer_list
 from typing import Tuple
+import socket
 
 class Server:
     def __init__(self, host: str, port: int):
@@ -22,9 +23,22 @@ class Server:
         """
         Starts the server to listen for client connections and requests.
         """
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_address = (self.host, self.port)
+        server_socket.bind(server_address)
+
+        server_socket.listen(1)
+        print(f"Server is up and running. At\nIP: {self.host}\nPort: {self.port}")
+        connection, client_address = server_socket.accept()
+        print("Connection from:", client_address)
+        data = connection.recv(1024)
+        print("Received:", data.decode())
+        connection.sendall("Hello, Client!".encode())
+        connection.close()
+        server_socket.close()
         # TODO: Initialize a socket server to listen on the specified port
         # HINT: Use multithreading to handle multiple client connections.
-        pass
+        # pass
 
     def handle_client_registration(self, client_id: str, client_ip: str) -> bool:
         """
